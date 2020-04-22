ee3f17f4ca4a18035c81b8a05d0d122ef5794e23
Konstantin <k.trepalin@yandex.ru>
2020-04-22 13:43:27 +0300

    card

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    session[:counter] ||= 0
+    session[:counter] +=1
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+	has_many :line_items
+
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
@@ -7,7 +9,21 @@ class Product < ApplicationRecord
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
 	}
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	def self.latest
 		Product.order(:updated_at).last
-	end	
+	end
+
+	private
+ # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'существуют товарные позиции')
+			return false
+		end
+	end
 end
